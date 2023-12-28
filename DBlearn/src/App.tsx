919e7ada42c2120b29b4563921d18b9077b1f1d9
djaber1000 <djabergasmig01@gmail.com>
2023-12-28 23:23:51 +0100

    first video

---
+++ DBlearn/src/App.tsx
@@ -0,0 +1,15 @@
+import React, { useEffect, useRef } from "react";
+
+function App() {
+  const ref = useRef<HTMLInputElement>(null);
+  useEffect(() => {
+    if (ref.current) ref.current.focus();
+  });
+  return (
+    <>
+      <input ref={ref} type="text" className="form-control" />
+    </>
+  );
+}
+
+export default App;
