bd86f753c8de00dab7b3819a07f286b76c3e7a92
djaber1000 <djabergasmig01@gmail.com>
2023-12-29 14:24:55 +0100

    use effect

--- DBlearn/src/App.tsx
+++ DBlearn/src/App.tsx
@@ -2,19 +2,20 @@ import React, { useEffect, useRef, useState } from "react";
 import ProductList from "./components/ProductList";
 
 function App() {
-  const [category , setCategory] = useState('')
-  
-  useEffect(() => {
-    
-  });
+  const [category, setCategory] = useState("");
+
+  useEffect(() => {});
   return (
     <>
-    <select className="from-select" onChange={(event) => setCategory(event.target.value)}>
-      <option value=""></option>
-      <option value="Clothing">Clothing</option>
-      <option value="Household">Household</option>
-    </select>
-    <ProductList category={category}/>
+      <select
+        className="from-select"
+        onChange={(event) => setCategory(event.target.value)}
+      >
+        <option value=""></option>
+        <option value="Clothing">Clothing</option>
+        <option value="Household">Household</option>
+      </select>
+      <ProductList category={category} />
     </>
   );
 }
