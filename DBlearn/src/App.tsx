3ca878fa02c0556742c61389a82e60315cbadf1c
djaber1000 <djabergasmig01@gmail.com>
2023-12-31 00:06:02 +0100

    final of backEnd

--- DBlearn/src/App.tsx
+++ DBlearn/src/App.tsx
@@ -1,26 +1,88 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
-
-interface User {
-  id: number;
-  name: string;
-}
+import "./App.css";
+import { CanceledError } from "./services/api-client";
+import userService, { User } from "./services/user-service";
+import useUsers from "./hooks/useUsers";
 
 function App() {
-  const [users, setUsers] = useState<User[]>([]);
-  const [error,setError]= useState()
-  useEffect(() => {
-    axios
-      .get<User[]>("https://jsonplaceholder.typicode.com/xusers")
-      .then((res) => setUsers(res.data))
-      .catch(err=>setError(err.message))
-  },[]);
+  const { users, error, isloading, setUsers, setError } = useUsers();
+
+  const deleteUser = (user: User) => {
+    const originalUser = [...users];
+    setUsers(users.filter((u) => u.id !== user.id));
+    userService.delete(user.id).catch((err) => {
+      if (err instanceof CanceledError) return;
+      setError(err.message);
+      setUsers(originalUser);
+    });
+  };
+
+  const addNewUser = () => {
+    const originalUser = [...users];
+    const newUser = { id: 0, name: "djaber" };
+    setUsers([...users, newUser]);
+    userService
+      .create(newUser)
+      .then(({ data: savedUser }) => {
+        setUsers([savedUser, ...users]);
+      })
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+        setUsers(originalUser);
+      });
+  };
+
+  const UpdateUser = (user: User) => {
+    const originalUser = [...users];
+    const UpdatedUser = { ...user, name: user.name + "!" };
+
+    setUsers(users.map((u) => (u.id === user.id ? UpdatedUser : u)));
+
+    userService.update(UpdatedUser).catch((err) => {
+      if (err instanceof CanceledError) return;
+      setError(err.message);
+      setUsers(originalUser);
+    });
+  };
   return (
     <div>
       {error && <p className="text-danger">{error}</p>}
-      {users.map((user) => (
-        <li key={user.id}>{user.name}</li>
-      ))}
+      {isloading && <div className="spinner-border"></div>}
+      <button
+        type="button"
+        className="btn btn-primary"
+        onClick={() => addNewUser()}
+      >
+        add
+      </button>
+      <ul className="list-group">
+        {users.map((user) => (
+          <li
+            className="list-group-item d-flex justify-content-between"
+            key={user.id}
+          >
+            {user.name}
+            <div>
+              <button
+                className="btn btn-outline-secondary mx-1 "
+                onClick={() => {
+                  UpdateUser(user);
+                }}
+              >
+                update
+              </button>
+              <button
+                className="btn btn-outline-danger "
+                onClick={() => {
+                  deleteUser(user);
+                }}
+              >
+                delete
+              </button>
+            </div>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
